d8b884a81667c27508a1e991c805fbb107a06a1e
hsy <2429377054@qq.com>
2022-08-12 16:13:43 +0800

    add login1

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -7,5 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/pk/")
 public class IndexController {
-
+    @RequestMapping("index/")
+    public String index() {return " ";}
 }
